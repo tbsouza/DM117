4d094045cbc55bad0eeee004b64a146b0fc224c8
Thiago Barbosa de Souza <tbsouza@outlook.com>
2018-06-21 22:02:30 -0300

    Desenv. do movimento da nave e criação do abacaxi. Telas de início e gameover funcionando

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,34 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 /// <summary>
 /// Classe para controlar o Player (nave)
 /// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
+
+    // enum do tipo de movimento da nave
+    private enum TipoMovimento { Mouse, Teclado }
 
     [SerializeField]
     [Tooltip("Velocidade de movimento da nave")]
-    [Range(1,10)]
-    private float velocidadeMovimento = 1;
+    [Range(5,20)]
+    private int velocidadeMovimento;
 
-    // Referencia para o componente rigidbody do Player
+    [SerializeField]
+    [Tooltip("Movimento por teclado ou mouse")]
+    private TipoMovimento movimentoHorizontal;
+
     private Rigidbody2D rb2d;
 
 
-	// Use this for initialization
-	void Start () {
-
-        // Pega o rb do Player
+    // Use this for initialization
+    void Start () {
         rb2d = GetComponent<Rigidbody2D>();
     }
 	
 
 	// Update is called once per frame
 	void Update () {
-
         movimentoNave();
-
     }
 
 
@@ -38,17 +40,33 @@ public class PlayerController : MonoBehaviour {
     /// </summary>
     private void movimentoNave() {
 
-        // Pega a direcao do movimento (Esq ou Dir) 
-        var velocidade = Input.GetAxis("Horizontal") * velocidadeMovimento;
+        // Verifica se esta utilizando teclado ou mouse
+        if (movimentoHorizontal == TipoMovimento.Mouse) {
 
-        // Adiciona forca na direcao do movimento desejado
-        rb2d.AddForce(new Vector2(velocidade, 0));
+            // Pega a posicao do mouse em unidades do mundo
+            float posicaoMovimento = (Input.mousePosition.x/Screen.width) * 16;
 
+            // Nova posicao da nave
+            Vector2 navePosition = new Vector2(0, transform.position.y);
 
+            // limite da nave dentro do cenario
+            navePosition.x = Mathf.Clamp(posicaoMovimento, 1.0f, 15.0f);
 
+            // Atualiza a posicao da nave
+            transform.position = navePosition;
+        }
+        else {
 
+            // velocidade de movimento da nave
+            var velocidadeLateral = Input.GetAxis("Horizontal") * velocidadeMovimento;
 
+            // adiciona forca na nava
+            rb2d.AddForce( new Vector2(velocidadeLateral, 0) );
+        }
+
+ 
     }
+
 
 
 }
